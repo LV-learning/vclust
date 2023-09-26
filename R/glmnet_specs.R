8decadf32d25fadb466d099dabb0ba4a3e74776e
zetan-li <139275407+zetan-li@users.noreply.github.com>
2023-09-26 16:05:28 -0700

    update glmnet_specs
    export glmnet_specs() function to be used for the program

--- R/glmnet_specs.R
+++ R/glmnet_specs.R
@@ -1,3 +1,8 @@
+#' Construct a glmnet_specs
+#'
+#' @return return a vector with glmnet parameters
+#' @export
+#'
 glmnet_specs <- function(...){
   if(is.null(names(c(...)))) stop("glmnet_specs should have names for each argument")
   c(...)
